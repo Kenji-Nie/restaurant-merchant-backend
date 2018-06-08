8db169fee0f67319ee4e849fda0bb55b5f67a17b
Kenji-Nie <kenyatta.nie@gmail.com>
2018-06-08 19:44:44 +0800

    在BaseService中增加外联查询

---
+++ typings/app/service/index.d.ts
@@ -0,0 +1,14 @@
+// This file was auto created by egg-ts-helper
+// Do not modify this file!!!!!!!!!
+
+import Base from '../../../app/service/base';
+import Home from '../../../app/service/home';
+import User from '../../../app/service/user';
+
+declare module 'egg' {
+  interface IService {
+    base: Base;
+    home: Home;
+    user: User;
+  }
+}
